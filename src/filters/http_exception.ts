7a62cf91498d6fba8f29c2f1d6963954b9284346
yang18391952502 <751334416@qq.com>
2022-06-07 19:29:40 +0800

    feature 新增全局异常处理过滤器

---
+++ src/filters/http_exception.ts
@@ -0,0 +1,37 @@
+import { Logger } from 'nestjs-pino'
+import moment from 'moment'
+
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common'
+import { Request, Response } from 'express'
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter<Error> {
+  constructor(private readonly logger: Logger) { console.log() }
+
+  public catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse<Response>()
+    const request = ctx.getRequest<Request>()
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR
+
+    const nowDate = moment().format('YYYY-MM-DD HH:mm:ss')
+    const errorResponse = {
+      state: status,
+      msg: exception.message,
+      data: {
+        error: exception.name,
+        date: nowDate,
+        path: request.url
+      }
+    }
+    this.logger.error(
+      `【${nowDate}】${request.method} ${request.url} query:${JSON.stringify(request.query)} params:${JSON.stringify(request.params)} body:${JSON.stringify(request.body)}`,
+      JSON.stringify(errorResponse),
+      'HttpExceptionFilter'
+    )
+
+    response.status(status).json(errorResponse)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from "./filters";
+import { Logger } from 'nestjs-pino'
 
 async function bootstrap() {
   /**
@@ -14,6 +16,10 @@ async function bootstrap() {
    * 设置全局路由前缀
    */
   app.setGlobalPrefix('axle')
+
+  // 全局异常捕捉过滤器
+  const logger = app.get(Logger)
+  app.useGlobalFilters(new HttpExceptionFilter(logger))
   await app.listen(3000);
 }
 bootstrap();
